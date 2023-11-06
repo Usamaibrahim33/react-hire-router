83ff6ddef514b3145de91ac0d6b051f10cceaeff
usama ibrahim <usamaibrahimusman@gmail.com>
2023-11-06 16:54:40 +0000

    completed core

--- src/pages/Dashboard/components/PeopleListItem.jsx
+++ src/pages/Dashboard/components/PeopleListItem.jsx
@@ -1,12 +1,20 @@
+/* eslint-disable react/prop-types */
+
+import { Link } from 'react-router-dom';
+
 function PeopleListItem(props) {
   const { person } = props
 
+  const personURL = `/john/${person.name.first}`;
   return (
-    <li>
+    <li className='peoples-card'>
       <h3>
         {person.name.first} {person.name.last}
       </h3>
       {person.wage && <p>Wage: £{person.wage}</p>}
+      
+       <Link to={personURL} 
+       state={person} >View more</Link>
     </li>
   )
 }
